4b7d8fc0eeda66f53d33432db90cb97789fc190f
Daniel Frehner <dfrehner1@gmail.com>
2018-04-24 22:05:52 -0600

    updated variable names

--- Article/Article.js
+++ Article/Article.js
@@ -1,8 +1,8 @@
 // Using jQuery, select all the items with the class of 'article'
-let items = $('.article');
+let articles = $('.article');
 
 // Here we are creating our new component class
-class Item {
+class Article {
   //the constructor will take an element as it's only argument
   constructor(element) {
     this.element = $(element);
@@ -28,4 +28,4 @@ class Item {
 
 // Use jQuery's .map function to map over the array of jQuery elements
 // Within .map, create a new instance of Article passing in each element to the constructor
-items = items.map((_, item) => new Item(item));
+articles = articles.map((_, item) => new Article(item));
