f88467cec4d5dff801836ac5322414b24f999ab2
John O'Rourke <officialjohnorourke@gmail.com>
2019-07-12 22:12:34 -0400

    Finishes menu mvp

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,11 +1,12 @@
 //Using jQuery, select the menu element and the menu-button element
-const menu;
-const menuButton;
+const menu = $('.menu');
+const menuButton = $('.menu-button');
 
 //Create a callback that will be passed to the menuButton click event listener
 //Inside this callback a class should be either added or removed from menu
 const toggleMenu = () => {
-
-}
+  menu.toggleClass('menu--open');
+};
 
 //Using jQuery, add a click event to the menuButton, passing it the toggleMenu function.
+menuButton.click(toggleMenu);
