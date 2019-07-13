3ae713f57243584ed52e060442804a51a2015fae
John O'Rourke <officialjohnorourke@gmail.com>
2019-07-12 22:28:25 -0400

    Finishes article mvp

--- Article/Article.js
+++ Article/Article.js
@@ -1,33 +1,41 @@
 /* Using jQuery, select all the article elements */
-let articles;
+let articles = $('.article');
 
 
 class Article {
-  /* The constructor will take an element as it's only argument */
+  /* The constructor will take an element as its only argument */
   constructor(element) {
     /* 
       We need to make sure the element being passed to us is a jQuery element.
       To do so, wrap the element in jQuery syntax. 
     */
-    this.element = $();
+    this.element = $(element);
     /* 
       Using our new reference to the element, find the expand button within 
       the element and set a new property on the Article class. 
     */
-    this.expandButton;
+    this.expandButton = this.element.find('.expandButton');
     /* 
       Using our new reference to the expandButton, use jQuery to set the inner
       text on the button. 
     */
-
+    this.expandButton.text('Expand');
     /* 
       Set a click handler on the expand button (or article element), calling 
       the expandArticle method. 
     */
+    this.expandButton.click(this.expandArticle.bind(this));
+    // ES6:
+    // this.expandButton.click(() => this.expandArticle());
   }
 
   expandArticle() {
     /* Using our reference to the article element, add or remove a class */
+    const open = 'article-open';
+    this.element.toggleClass(open);
+    if (this.element.hasClass(open)) {
+      this.expandButton.text('Collapse');
+    } else this.expandButton.text('Expand');
   }
 }
 
@@ -36,4 +44,4 @@ class Article {
   Within .map, we create a new instance of Article passing in each article element 
   to the constructor
 */
-articles = articles.map();
+articles = articles.map((i, article) => new Article(article));
